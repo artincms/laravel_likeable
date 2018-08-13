098336264c37749cc184390e95d09fee77bf0b7e
faramarz sadeghi <faramarz.sadeghi89@gmail.com>
2018-08-13 16:54:59 +0430

    likeable

--- src/Components/laravel_likeable_system.vue
+++ src/Components/laravel_likeable_system.vue
@@ -1,7 +1,11 @@
 <template>
-    <div class="float-right">
-        <i v-if="type ==='like'" class="far fa-thumbs-up color_green pointer" @click="changeLike">{{like}}</i>
-        <i v-else-if="type ==='disLike'" class="far fa-thumbs-down color_red pointer" @click="changeLike">{{dis_like}}</i>
+    <div class="float-right" v-if="canVote">
+        <i class="far fa-thumbs-up color_green pointer" @click="changeLike('like')" :class="{bold:isActiveLike}">{{like}}</i>
+        <i class="far fa-thumbs-down color_red pointer" @click="changeLike('disLike')" :class="{bold:isActiveDislike}">{{dis_like}}</i>
+    </div>
+    <div class="float-right" v-else>
+        <i v-if="type ==='like'" class="far fa-thumbs-up color_gray ">{{like}}</i>
+        <i v-else-if="type ==='disLike'" class="far fa-thumbs-down color_gray">{{dis_like}}</i>
         <i v-else></i>
     </div>
 </template>
@@ -13,70 +17,79 @@
         name: "laravel_likeable_system",
         props: ['model', 'item', 'type'],
         data: function () {
-            return {}
-        },
-        computed: {
-            like: {
-                get: function () {
-                    if (this.item.likes) {
-                        return this.item.likes.length;
-                    }
-                    else {
-                        return 0;
-                    }
-                },
-            },
-            dis_like: function () {
-                if (this.item.dis_likes) {
-                    return this.item.dis_likes.length;
-                }
-                else {
-                    return 0;
-                }
+            return {
+                canVote:true,
+                isActiveLike:false,
+                isActiveDislike:false,
+                like :this.item.likes.length,
+                dis_like:this.item.dis_likes.length
             }
         },
+        computed: {
+        },
         methods: {
-            changeLike: function () {
-                axios.post("/LLS/chnageLike", {encode_id: this.item.encode_id, model: this.model, type: this.type}).then(response => {
+            changeLike: function (type) {
+                axios.post("/LLS/chnageLike", {encode_id: this.item.encode_id, model: this.model, type: type,isActiveLike:this.isActiveLike,isActiveDislike:this.isActiveDislike}).then(response => {
                     this.$nextTick(() => {
                         if (response.data.success) {
-                            if (response.data.type == 'like') {
-                               if(this.item.likes)
-                               {
-                                   this.item.likes.push([]) ;
-                               }
-                               else
-                               {
-                                   this.item.likes=[
-                                            {0:'1'}
-                                       ];
-                               }
-
-                            }
-                            else if (response.data.type == 'disLike') {
-                                if(this.item.dis_likes)
+                            if (type == 'like')
+                            {
+                                if(this.isActiveLike)
                                 {
-                                    this.item.dis_likes.push([]) ;
+                                   this.like --;
                                 }
                                 else
                                 {
-                                    this.item.dis_likes=[
-                                        {0:'1'}
-                                    ];
+                                    if(this.isActiveDislike)
+                                    {
+                                       this.like ++ ;
+                                       this.dis_like -- ;
+                                        this.isActiveDislike = false;
+                                    }
+                                    else
+                                    {
+                                        this.like ++;
+                                    }
                                 }
+                                this.isActiveLike = !this.isActiveLike;
+                            }
+                            else if (type == 'disLike') {
+                                if (this.isActiveDislike)
+                                {
+                                    this.dis_like -- ;
+                                }
+                                else
+                                {
+                                    if (this.isActiveLike)
+                                    {
+                                        this.dis_like ++ ;
+                                        this.like --;
+                                        this.isActiveLike = false;
+
+                                    }
+                                    else
+                                    {
+                                        this.dis_like ++ ;
+                                    }
+                                }
+
+                                this.isActiveDislike = !this.isActiveDislike;
                             }
                             else {
                                 console.log('you should chose type equal like or disLkie');
                             }
                         }
-                        else {
-                            alert('Please check console log');
-                            console.log(this.item);
+                        else
+                        {
+                            console.log(response.data);
                         }
-
                     })
                 })
             },
+
+            setVote:function () {
+
+            }
         }
     }
 </script>
@@ -91,4 +104,11 @@
     .float-right{
         float:left!important;
     }
+    .color_gray{
+        color: #808080;
+    }
+    .bold {
+        font-weight: bold;
+    }
+
 </style>
--- src/Controllers/LikeController.php
+++ src/Controllers/LikeController.php
@@ -17,6 +17,58 @@ class LikeController extends Controller
         $id = LFM_GetDecodeId($request->encode_id);
         $model = $request->model;
         $type = $request->type;
+        $isActiveLike = $request->isActiveLike;
+        $isActiveDislike = $request->isActiveDislike;
+        $ip=$request->ip();
+        if($type =='like')
+        {
+            $type_id= '1';
+            if($isActiveLike)
+            {
+                $this->deleteLike($model,$id,$type_id) ;
+                $result['action']='deccreamentLike';
+            }
+            elseif ($isActiveDislike)
+            {
+                $this->deleteLike($model,$id,'-1','disLikes') ;
+                $this->setLike($model,$id,$type_id,$ip);
+                $result['action']='increamentLike';
+            }
+            else
+            {
+                $this->setLike($model,$id,$type_id,$ip);
+                $result['action']='increamentLike';
+            }
+
+        }
+        else
+        {
+           $type_id = '-1';
+            if($isActiveDislike)
+            {
+                $this->deleteLike($model,$id,$type_id,'disLikes') ;
+                $result['action']='deccreamentDislike';
+            }
+            elseif ($isActiveLike)
+            {
+                $this->deleteLike($model,$id,'1') ;
+                $this->setLike($model,$id,$type_id,$ip);
+                $result['action']='increamentDislike';
+            }
+            else
+            {
+                $this->setLike($model,$id,$type_id,$ip);
+                $result['action']='increamentDislike';
+            }
+        }
+
+        $result['success']=true ;
+        $result['type']=$type ;
+        return $result;
+    }
+
+    public function setLike($model,$id,$type,$ip)
+    {
         $item = new Like;
         if (Auth::user())
         {
@@ -26,23 +78,22 @@ class LikeController extends Controller
             }
 
         }
-        $item->ip = $request->ip() ;
+        $item->ip = $ip ;
         $item->target_id = $id;
         $item->target_type = $model;
-        if($request->type =='like')
-        {
-            $item->type = '1';
-        }
-        elseif($request->type =='disLike')
-        {
-            $item->type = '-1';
-        }
-
+        $item->type = $type;
         $item->save();
         $result = [
             'success'  => true,
-            'type'     => $type,
         ];
         return $result;
+    }
+
+    public function deleteLike($model,$id,$type,$relation_name='likes')
+    {
+        $item=$model::find($id);
+        $likes=$item->$relation_name()->where('type',$type)->orderBy('id','desc')->first();
+        $likes->delete();
+        return true ;
     }
 }
