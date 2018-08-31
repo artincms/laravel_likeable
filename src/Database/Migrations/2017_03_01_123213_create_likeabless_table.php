9e19641cf8c091cc03ae43e7f1f022f53eba92e7
faramarz <faramarz.sadeghi89@gmail.com>
2018-08-31 11:11:21 +0430

    debug

--- src/Database/Migrations/2017_03_01_123213_create_likeabless_table.php
+++ src/Database/Migrations/2017_03_01_123213_create_likeabless_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLikeablesTable extends Migration
+class CreateLikeablessTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,12 +15,12 @@ class CreateLikeablesTable extends Migration
     {
         Schema::create('lls_likes', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned()->default(0);
+            $table->integer('user_id')->unsigned()->nullable()->default(0);
             $table->string('ip')->nullable()->default(null);
             $table->integer('target_id')->unsigned();
             $table->string('target_type', 255);
             $table->enum('type',['1','-1'])->nullable()->default('1');
-            $table->integer('created_by')->unsigned()->default(0);
+            $table->integer('created_by')->unsigned()->nullable()->default(0);
             $table->timestamps();
             $table->softDeletes();
         });
--- src/Traits/LaraveLikeablesSystem.php
+++ src/Traits/LaraveLikeablesSystem.php
@@ -1,22 +1,7 @@
 <?php
 namespace App\Traits ;
 
-
-use ArtinCMS\LCS\Model\Comment;
-
 trait LaraveLikeablesSystem {
-
-    function setDataTableColumn($data)
-    {
-        $model = $data->target_type ;
-        $res = $model::find($data->target_id);
-        //name of column you want show in admin
-        $result['text'] = $res->title;
-
-        //your path comment
-        $result['url'] = route('vue');
-        return $result ;
-    }
 
     public function likes()
     {
