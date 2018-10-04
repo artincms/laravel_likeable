1827b9923e05f5f2ddaea13f003383e0a3125a19
adel <adel.raheli@gmail.com>
2018-10-05 01:20:28 +0330

    1

--- src/Config/LLS.php
+++ src/Config/LLS.php
@@ -3,7 +3,7 @@
 return [
 
     /* Important Settings */
-    'backend_lls_middlewares'   => explode(',', env('BACKEND_MIDDLEWARES', 'web')),
+    'backend_lls_middlewares'   => explode(',', env('LLS_BACKEND_MIDDLEWARES', 'web')),
     'frontend_lls_middlewares'  => explode(',', env('LLS_FRONTEND_MIDDLEWARES', 'web', 'throttle:60,1')),
     // you can change default route from sms-admin to anything you want
     'backend_lls_route_prefix'  => env('LLS_BACKEND_ROUTE_PERFIX', 'LLS'),
