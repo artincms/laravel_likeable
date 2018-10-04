2f534cada65809e0ce35f905e1da7135d79be81b
adel <adel.raheli@gmail.com>
2018-10-05 00:21:56 +0330

    middleware .env as array supported

--- src/Config/LLS.php
+++ src/Config/LLS.php
@@ -3,10 +3,10 @@
 return [
 
     /* Important Settings */
-    'backend_lls_middlewares' =>  env('BACKEND_LLS_MIDDLEWARES', 'web'),
-    'frontend_lls_middlewares' => env('FRONTEND_LLS_MIDDLEWARES', 'web','throttle:60,1'),
+    'backend_lls_middlewares'   => explode(',', env('BACKEND_LLS_MIDDLEWARES', 'web')),
+    'frontend_lls_middlewares'  => explode(',', env('FRONTEND_LLS_MIDDLEWARES', 'web', 'throttle:60,1')),
     // you can change default route from sms-admin to anything you want
-    'backend_lls_route_prefix' =>  env('BACKEND_LLS_ROUTE_PERFIX', 'LLS'),
+    'backend_lls_route_prefix'  => env('BACKEND_LLS_ROUTE_PERFIX', 'LLS'),
     'frontend_lls_route_prefix' => env('FRONTEND_LLS_ROUTE_PERFIX', 'LLS'),
     // ======================================================================
     'user_model'                => env('LLS_USER_MODEL', 'App\User'),
