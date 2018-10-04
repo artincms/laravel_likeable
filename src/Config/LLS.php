d3ff1aac628db249b2238a29d906ffbe944e46ee
adel <adel.raheli@gmail.com>
2018-10-05 00:37:40 +0330

    config change and fixed issue

--- src/Config/LLS.php
+++ src/Config/LLS.php
@@ -3,11 +3,11 @@
 return [
 
     /* Important Settings */
-    'backend_lls_middlewares'   => explode(',', env('BACKEND_LLS_MIDDLEWARES', 'web')),
-    'frontend_lls_middlewares'  => explode(',', env('FRONTEND_LLS_MIDDLEWARES', 'web', 'throttle:60,1')),
+    'backend_lls_middlewares'   => explode(',', env('BACKEND_MIDDLEWARES', 'web')),
+    'frontend_lls_middlewares'  => explode(',', env('LLS_FRONTEND_MIDDLEWARES', 'web', 'throttle:60,1')),
     // you can change default route from sms-admin to anything you want
-    'backend_lls_route_prefix'  => env('BACKEND_LLS_ROUTE_PERFIX', 'LLS'),
-    'frontend_lls_route_prefix' => env('FRONTEND_LLS_ROUTE_PERFIX', 'LLS'),
+    'backend_lls_route_prefix'  => env('LLS_BACKEND_ROUTE_PERFIX', 'LLS'),
+    'frontend_lls_route_prefix' => env('LLS_FRONTEND_ROUTE_PERFIX', 'LLS'),
     // ======================================================================
     'user_model'                => env('LLS_USER_MODEL', 'App\User'),
 //    'Trait' => [
